366471c080916f0e8df455ffd7c456ebc56fda38
ashlee.larrick <ashleemareelarrick323@gmail.com>
2023-04-24 09:56:05 -0600

    event target added

--- src/index.js
+++ src/index.js
@@ -9,29 +9,6 @@ function showAllFish(event) {
             list = document.getElementById('fish-list')
             while (list.firstChild) {
                 list.removeChild(list.firstChild);
-              }
-            const fish = event.target.value.toLowerCase()
-            for(const index in data) {
-                    let li = document.createElement("li")
-                    const image = document.createElement("img")
-                    image.src = data[index].image
-                    li.appendChild(image)
-                    const p = document.createElement("p")
-                    p.innerHTML = data[index].name
-                    li.appendChild(p)
-                    const btn = document.createElement("button")
-                    btn.innerHTML = "See More"
-                    li.appendChild(btn)
-                    btn.addEventListener("click",() => {
-                        const p = document.createElement("p")
-                    p.innerHTML = data[index].seasons.toString() + 
-                    ', ' + data[index].weather + ', ' + data[index].time + ', ' + data[index].location
-                    li.appendChild(p)
-                    } )
-                    list.appendChild(li)
-            }
-        }
-        )
 }
 
 function showFish(event) {
