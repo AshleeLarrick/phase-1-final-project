b50e7613761cb6fcfe3075fe2ae2bb2b66d7d375
ashlee.larrick <ashleemareelarrick323@gmail.com>
2023-04-24 09:52:17 -0600

    added fetch

--- src/index.js
+++ src/index.js
@@ -3,7 +3,35 @@ document.getElementById("see-all-btn").addEventListener("click", showAllFish)
 const url = "http://localhost:3000/fish"
 
 function showAllFish(event) {
-    
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            list = document.getElementById('fish-list')
+            while (list.firstChild) {
+                list.removeChild(list.firstChild);
+              }
+            const fish = event.target.value.toLowerCase()
+            for(const index in data) {
+                    let li = document.createElement("li")
+                    const image = document.createElement("img")
+                    image.src = data[index].image
+                    li.appendChild(image)
+                    const p = document.createElement("p")
+                    p.innerHTML = data[index].name
+                    li.appendChild(p)
+                    const btn = document.createElement("button")
+                    btn.innerHTML = "See More"
+                    li.appendChild(btn)
+                    btn.addEventListener("click",() => {
+                        const p = document.createElement("p")
+                    p.innerHTML = data[index].seasons.toString() + 
+                    ', ' + data[index].weather + ', ' + data[index].time + ', ' + data[index].location
+                    li.appendChild(p)
+                    } )
+                    list.appendChild(li)
+            }
+        }
+        )
 }
 
 function showFish(event) {
