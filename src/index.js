e8d3a84b736afdd18efabfb115503ff2c9b260dd
ashlee.larrick <ashleemareelarrick323@gmail.com>
2023-04-24 07:38:21 -0600

    added images and see more button

--- src/index.js
+++ src/index.js
@@ -17,7 +17,23 @@ function showFish(event) {
             for(const index in data) {
                 if(data[index].name.toLowerCase().startsWith(fish)) {
                     let li = document.createElement("li")
-                    li.innerHTML = data[index].name
+                    const image = document.createElement("img")
+                    image.src = data[index].image
+                    li.appendChild(image)
+                    const p = document.createElement("p")
+                    p.innerHTML = data[index].name
+                    li.appendChild(p)
+                    const btn = document.createElement("button")
+                    btn.innerHTML = "See More"
+                    li.appendChild(btn)
+                    btn.addEventListener("click",() => {
+                        const p = document.createElement("p")
+                    p.innerHTML = data[index].seasons.toString() + 
+                    ', ' + data[index].weather + ', ' + data[index].time + ', ' + data[index].location
+                    li.appendChild(p)
+                    } )
+                    /*li.innerHTML = data[index].name + ': ' + data[index].seasons.toString() + 
+                    ', ' + data[index].weather + ', ' + data[index].time + ', ' + data[index].location */
                     list.appendChild(li)
                 }
             }
