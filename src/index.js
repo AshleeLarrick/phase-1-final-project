8456843c0e7fc0c46d5a7badb27bf6f4f2563308
ashlee.larrick <ashleemareelarrick323@gmail.com>
2023-05-08 08:19:54 -0600

    New funcions

--- src/index.js
+++ src/index.js
@@ -2,50 +2,56 @@ document.getElementsByName("name")[0].addEventListener("keypress", showFish)
 document.getElementById("see-all-btn").addEventListener("click", showAllFish)
 const url = "http://localhost:3000/fish"
 let fish = '';
+const data = []
 
-function fetchFish(event, callback) {
+function fetchAll() {
     fetch(url)
         .then(response => response.json())
-        .then(data => {
-            list = document.getElementById('fish-list')
-            while (list.firstChild) {
-                list.removeChild(list.firstChild);
-            }
-            console.log(typeof(callback.name));
-            if (callback.name === 'callbackShowFish') {
-                console.log(typeof(callback));
-            fish = event.target.value.toLowerCase()}
-            data.forEach(callback)})
+        .then(datacopy => datacopy.forEach(d => data.push(d)))
+}
+
+function fetchFish(event, callback) {
+
+    list = document.getElementById('fish-list')
+    while (list.firstChild) {
+        list.removeChild(list.firstChild);
+    }
+    console.log(typeof (callback.name));
+    if (callback.name === 'callbackShowFish') {
+        console.log(typeof (callback));
+        fish = event.target.value.toLowerCase()
+    }
+    data.forEach(callback)
 }
 
 function callbackShowAllFish(element) {
-        let li = document.createElement("li")
-        const image = document.createElement("img")
-        image.src = element.image
-        li.appendChild(image)
+    let li = document.createElement("li")
+    const image = document.createElement("img")
+    image.src = element.image
+    li.appendChild(image)
+    const p = document.createElement("p")
+    p.innerHTML = element.name
+    li.appendChild(p)
+    const btn = document.createElement("button")
+    btn.innerHTML = "See More"
+    li.appendChild(btn)
+    btn.addEventListener("dblclick", () => {
         const p = document.createElement("p")
-        p.innerHTML = element.name
+        p.innerHTML = element.seasons.toString() +
+            ', ' + element.weather + ', ' + element.time + ', ' + element.location
+        if (li.lastChild.nodeName === 'P') {
+            li.removeChild(li.lastChild)
+        }
         li.appendChild(p)
-        const btn = document.createElement("button")
-        btn.innerHTML = "See More"
-        li.appendChild(btn)
-        btn.addEventListener("dblclick", () => {
-            const p = document.createElement("p")
-            p.innerHTML = element.seasons.toString() +
-                ', ' + element.weather + ', ' + element.time + ', ' + element.location
-            if (li.lastChild.nodeName === 'P') {
-                li.removeChild(li.lastChild)
-            }
-            li.appendChild(p)
-        })
-        list.appendChild(li)
-    };
+    })
+    list.appendChild(li)
+};
 
 function showAllFish(event) {
     fetchFish(event, callbackShowAllFish)
 }
 
-function callbackShowFish (element) {
+function callbackShowFish(element) {
     if (element.name.toLowerCase().startsWith(fish)) {
         let li = document.createElement("li")
         const image = document.createElement("img")
@@ -75,5 +81,6 @@ function showFish(event) {
         event.preventDefault()
         console.log(event)
         fetchFish(event, callbackShowFish)
-    } 
+    }
 }
+fetchAll()
